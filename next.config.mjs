c79cdedc94f70ec946d91dd7cac842125dbfc8e2
IgorAugust0 <augustoigor269@gmail.com>
2024-09-29 16:34:37 -0300

    feat(docs): add cards nas paginas de etapas com icones

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,30 @@
-import nextra from 'nextra'
- 
+import path from "node:path";
+import nextra from "nextra";
+
+// to deploy with nginx, github pages, etc, read: https://nextra.site/docs/guide/static-exports
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx'
-})
- 
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+const sep = path.sep === "/" ? "/" : "\\\\";
+const ALLOWED_SVG_REGEX = new RegExp(`components${sep}icons${sep}.+\\.svg$`);
+
 export default withNextra({
-//   i18n: {
-//     locales: ['pt_BR', 'en-US', 'es'],
-//     defaultLocale: 'pt_BR'
-//   }
-})
+  //   i18n: {
+  //     locales: ['pt_BR', 'en-US', 'es'],
+  //     defaultLocale: 'pt_BR'
+  //   }
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+    fileLoaderRule.exclude = ALLOWED_SVG_REGEX;
+
+    config.module.rules.push({
+      test: ALLOWED_SVG_REGEX,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+});
